e2d63e4785e9341e749398617d05f831f2513f55
mdussali <mdussali@student.42vienna.com>
2023-09-08 14:32:02 +0200

    ft_memset, b_zero added

--- ft_memset.c
+++ ft_memset.c
@@ -1,1 +1,16 @@
 #include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char		*temp;
+    size_t i;
+	
+    temp = (unsigned char *)s;
+    i = 0;
+	while (i < n)
+	{
+		*(temp + i) = (unsigned char)c;
+		i++;
+	}
+    return (s);
+}
